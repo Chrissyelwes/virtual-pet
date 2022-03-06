bbdc0eb790525542e285099e63615e009e66a148
Chrissy Elwes <96088605+Chrissyelwes@users.noreply.github.com>
2022-03-06 12:12:23 +0000

    hunger and fitness added

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -16,8 +16,19 @@ describe('constructor', () => {
 
 describe('growUp', () => {
     it('increments the age by 1', () => {
-        const pet = new Pet('Fido');
+        const pet = new Pet('Cat');
         pet.growUp();
         expect(pet.age).toEqual(1);
     });
+    it('increases hunger by 5', () => {
+        const pet = new Pet('Cat');
+        pet.growUp();
+        expect(pet.hunger).toEqual(5);
+    });
+    it('decreses fitness by 3', () => {
+        const pet = new Pet('Cat');
+        pet.growUp();
+        expect(pet.fitness).toEqual(7);
+    });
 });
+
