26a331cf3644878bb612867095b4c45124491a3d
Chrissy Elwes <96088605+Chrissyelwes@users.noreply.github.com>
2022-03-06 11:51:24 +0000

    growUp added as method

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -2,10 +2,22 @@ const Pet = require('../src/pet');
 
 describe('constructor', () => {
     it('returns an object', () => {
-        expect(new Pet('Fido')).toBeInstanceOf(Object);
+        expect(new Pet('Cat')).toBeInstanceOf(Object);
     });
     it('sets the name property', () => {
+        const pet = new Pet('Cat');
+        expect(pet.name).toEqual('Cat');
+    });
+    it('has a initial age of 0', () => {
+        const pet = new Pet('Cat');
+        expect(pet.age).toEqual(0);
+    });
+});
+
+describe('growUp', () => {
+    it('increments the age by 1', () => {
         const pet = new Pet('Fido');
-        expect(pet.name).toEqual('Fido');
+        pet.growUp();
+        expect(pet.age).toEqual(1);
     });
 });
